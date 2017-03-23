88bbf0ed029ef43452849a1b8039b4f2c4bca79d
El Sonny <sonny@spaceshiplabs.com>
2017-03-23 12:46:01 +0100

    minimum prototype

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,20 +8,27 @@
  * Controller of the mediosSuciosFrontApp
  */
 angular.module('mediosSuciosFrontApp')
-  .controller('MainCtrl', function(msApiService, metadataService) {
+  .controller('MainCtrl', function(msApiService, metadataService, $mdSidenav) {
 
     var vm = this;
 
+    vm.loadingReports = false;
     vm.report = {};
     vm.reporting = false;
     vm.reports = [];
 
-    vm.init = init;
     vm.getReports = getReports;
+    vm.init = init;
     vm.setReports = setReports;
     vm.submitReport = submitReport;
-
+    vm.toggleSidenav = toggleSidenav();
     vm.init();
+
+
+    function getReports() {
+      vm.loadingReports = true;
+      return msApiService.getReports().then(vm.setReports);
+    }
 
     function init() {
       vm.getReports();
@@ -36,11 +43,10 @@ angular.module('mediosSuciosFrontApp')
       ];
     }
 
-    function getReports() {
-      return msApiService.getReports().then(vm.setReports);
-    }
+
 
     function setReports(reports) {
+      vm.loadingReports = false;
       vm.reports = reports.map(function(report) {
         report.info = metadataService.getBasicInfo(report.metadata);
         return report;
@@ -56,6 +62,13 @@ angular.module('mediosSuciosFrontApp')
           vm.metadata = report;
           vm.getReports();
         });
+    }
+
+    function toggleSidenav() {
+      return function() {
+      console.log('toggling');
+        $mdSidenav('left').toggle();
+      };
     }
 
 
